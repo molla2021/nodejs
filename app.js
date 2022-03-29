933aa3045bdfc114064a637e0d57fc7ed1ddb9f2
molla2021 <moget994@gmail.com>
2022-03-29 17:50:22 +0300

    express router added

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-const server = http.createServer(app);
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-server.listen(3000);
+app.listen(3000);
